6ad163f0d27568b52ff65a5bb1fb38c93e2d8f75
dell <dell@ENG-ESSRAA>
2018-10-26 14:22:37 -0700

    COMMIT

---
+++ MAV3/src/test/java/TESTS/TC001.java
@@ -0,0 +1,15 @@
+package TESTS;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC001{
+	@Test
+	public void dummy(){
+		
+		
+		Assert.assertTrue(true);
+	}
+	
+	
+}
